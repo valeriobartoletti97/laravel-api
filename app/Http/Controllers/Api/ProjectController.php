3577750714159059372ee6ee57aed35af90d2320
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2024-01-21 15:09:14 +0100

    added projects api call

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    //
+    public function index(){
+        $project = Project::all();
+        return response()->json([
+            'success' => true,
+            'data' => $project
+        ]);
+    }
+    public function show($id){
+        $project = Project::where('id', $id)->with(['technologies', 'type'])->get();
+        return response()->json([
+            'success' => true,
+            'data' => $project
+        ]);
+    }
+}
